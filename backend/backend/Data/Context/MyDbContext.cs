96f21c8e9400cf651fd477a89c91a22e6db5bfb2
milospejic <milospejic2001@gmail.com>
2024-04-06 14:56:32 +0200

    Odradjena konekcija sa bazom podataka

---
+++ backend/backend/Data/Context/MyDbContext.cs
@@ -0,0 +1,52 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace backend.Data.Context
+{
+    public class MyDbContext : DbContext
+    {
+        private readonly IConfiguration configuration;
+
+        public MyDbContext(DbContextOptions<MyDbContext> options, IConfiguration configuration) : base(options)
+        {
+            this.configuration = configuration;
+        }
+
+        public DbSet<Review> ReviewTable { get; set; }
+
+        public DbSet<ProductOrder> ProductOrderTable { get; set; }
+
+        public DbSet<Order> OrderTable { get; set; }
+        public DbSet<Product> ProductTable { get; set; }
+
+        public DbSet<Discount> DiscountTable { get; set; }
+
+        public DbSet<User> UserTable { get; set; }
+
+        public DbSet<Address> AddressTable { get; set; }
+
+        public DbSet<Admin> AdminTable { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("BasketballDBConnection"));
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+
+        }
+
+    }
+
+
+}
+
+
+
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,11 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+using backend.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Dodajte registraciju AutoMapper-a
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder => builder.WithOrigins("http://localhost:5242"));
+});
+
+builder.Services.AddDbContext<MyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BasketballDBConnection"))
+    .LogTo(Console.WriteLine, LogLevel.Information);
+});
 
 var app = builder.Build();
 
