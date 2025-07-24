58998881f31d412403c5de434aec1ec551979d41
milospejic <milospejic2001@gmail.com>
2025-07-24 15:19:28 +0200

    angular.json quickfix v3

--- frontend/src/app/environments/environment.prod.ts
+++ frontend/src/app/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: '__API_URL_PLACEHOLDER__'
+  apiUrl: '__API_URL_PLACEHOLDER__',
+  stripe: {
+      publishableKey: 'pk_test_51PNBZhP8wWiptW3jZO40HP4vFRUSuGQjI1Utc1WRzHqtZh0eocdPp3ZOlbvGI4eurYUwUjKe1o4nCQLizcwsIqiy00wBysgGEg'
+  }
 };
